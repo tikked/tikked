aaa1f8aaa98147f742532e72724c64eb0efd5b1d
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2021-10-07 23:35:50 +0200

    Upgrade tailwind version

--- packages/tikked-ui/postcss.config.js
+++ packages/tikked-ui/postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    require('postcss-custom-properties'),
-    require('autoprefixer')
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 };

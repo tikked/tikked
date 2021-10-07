46f93784b604afb6694613fdde77b536d96141d1
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2021-10-07 23:36:18 +0200

    Enable server passed env configuration

--- packages/tikked-ui/src/main.js
+++ packages/tikked-ui/src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
+
+import App from './App.vue';
+import env from './plugins/env';
+import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-import App from './App.vue';
-import router from './router';
-
 import '@/assets/css/main.css';
 
-createApp(App).use(router).use(VueAxios, axios).mount('#app');
+const app = createApp(App);
+app.use(env);
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount('#app');
---
+++ packages/tikked-ui/src/plugins/env.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+export const envSymbol = Symbol();
+
+export default {
+  install: app => {
+    app.config.globalProperties.$env = serverConfig;
+    app.provide(envSymbol, serverConfig);
+  }
+};

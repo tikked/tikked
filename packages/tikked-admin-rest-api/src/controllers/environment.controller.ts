28d968f2997e8d81434df2c54b2f68ea386798fa
Alex Bondo Andersen <abondoa@gmail.com>
2023-10-14 23:15:30 +0000

    Reuse existing function to return env. Gets rid of status call.

--- packages/tikked-admin-rest-api/src/controllers/environment.controller.ts
+++ packages/tikked-admin-rest-api/src/controllers/environment.controller.ts
@@ -2,27 +2,27 @@ import { Request, Response } from 'express';
 import { readFileSync, readdirSync, writeFileSync, unlinkSync } from 'fs';
 
 export class EnvironmentController {
-  getAll(request: Request, response: Response) {
-    response.status(200).send(
+  getAll = (request: Request, response: Response) => {
+    response.send(
       readdirSync("../../samples")
       .map(x => ({ 
         id: x.replace('.json', ''),
         href: `http://localhost:4322/nest/${x.replace('.json', '')}`
       })));
   }
-  getOne(request: Request, response: Response) {
-    response.status(200).send(readFileSync(`../../samples/${request.params.envId}.json`));
+  getOne = (request: Request, response: Response) => {
+    response.send(JSON.parse(readFileSync(`../../samples/${request.params.envId ?? request.body.id}.json`).toString()));
   }
-  createOne(request: Request, response: Response) {
+  createOne = (request: Request, response: Response) => {
     writeFileSync(`../../samples/${request.body.id}.json`, JSON.stringify(request.body));
-    response.status(200).send(readFileSync(`../../samples/${request.body.id}.json`));
+    return this.getOne(request, response);
   }
-  updateOne(request: Request, response: Response) {
+  updateOne = (request: Request, response: Response) => {
     writeFileSync(`../../samples/${request.params.envId}.json`,JSON.stringify(request.body))
-    response.status(200).send(readFileSync(`../../samples/${request.params.envId}.json`));
+    return this.getOne(request, response);
   }
-  deleteOne(request: Request, response: Response) {
+  deleteOne = (request: Request, response: Response) => {
     unlinkSync(`../../samples/${request.params.envId}.json`);
-    response.status(200).send();
+    response.send();
   }
 }

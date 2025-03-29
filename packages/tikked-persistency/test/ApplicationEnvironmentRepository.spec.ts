15703a293fa684965687160cab771567b1e000ef
Alex Bondo Andersen <abondoa@gmail.com>
2025-03-29 23:24:33 +0000

    Using sinon more directly for stubbing functions

--- packages/tikked-persistency/test/ApplicationEnvironmentRepository.spec.ts
+++ packages/tikked-persistency/test/ApplicationEnvironmentRepository.spec.ts
@@ -3,13 +3,12 @@ import chaiAsPromised from 'chai-as-promised';
 import { firstValueFrom, lastValueFrom, Observable, of } from 'rxjs';
 import { SinonStub, stub } from 'sinon';
 import sinonChai from 'sinon-chai';
-import {stubInterface} from '@salesforce/ts-sinon';
+import {StubbedType, stubInterface} from '@salesforce/ts-sinon';
 import * as sinon from 'sinon';
 import { ApplicationEnvironment } from '@tikked/core';
 import { Coder, DataStream, StreamFactory } from '../src/persistency';
 import { ApplicationEnvironmentRepository } from '../src/persistency/ApplicationEnvironmentRepository';
 import { createApplicationEnvironment, createId } from './Fixture';
-import { triggerAsyncId } from 'async_hooks';
 chaiUse(sinonChai);
 chaiUse(chaiAsPromised);
 
@@ -37,28 +36,23 @@ describe('ApplicationEnvironmentRepository', () => {
     describe('given a persisted application environment', () => {
       let id: string;
       let appEnv: ApplicationEnvironment;
-      let stubbedStream: DataStream;
-      let stubbedErrorHandler: SinonStub;
-      let stubbedStreamFactory: StreamFactory<DataStream>;
-      let stubbedCoder: Coder<string>;
       let repo: ApplicationEnvironmentRepository;
-      let create: (inId: string) => DataStream<string>
-      let decode: (code: string) => ApplicationEnvironment
-      let read: () => Observable<string>
+      let stubbedStream: StubbedType<DataStream>;
+      let stubbedErrorHandler: SinonStub;
+      let stubbedStreamFactory: StubbedType<StreamFactory<DataStream>>;
+      let stubbedCoder: StubbedType<Coder<string>>;
 
       beforeEach(() => {
         // Arrange
         id = createId();
         appEnv = createApplicationEnvironment(id);
         stubbedErrorHandler = stub();
-        read = () => of('appEnv')
-        stubbedStream = stubInterface<DataStream>(sinon,{ read: () => read() });
-        create = () => stubbedStream;
-        stubbedStreamFactory = stubInterface<StreamFactory<DataStream>>(sinon,{
-          create: (inId) => create(inId)
-        });
-        decode = () => appEnv
-        stubbedCoder = stubInterface<Coder<string>>(sinon,{ decode: (code) => decode(code) });
+        stubbedStream = stubInterface<DataStream>(sinon);
+        stubbedStream.read.returns(of('appEnv'))
+        stubbedStreamFactory = stubInterface<StreamFactory<DataStream>>(sinon);
+        stubbedStreamFactory.create.returns(stubbedStream);
+        stubbedCoder = stubInterface<Coder<string>>(sinon);
+        stubbedCoder.decode.returns(appEnv);
         repo = new ApplicationEnvironmentRepository(
           stubbedStreamFactory,
           stubbedCoder,
@@ -76,7 +70,7 @@ describe('ApplicationEnvironmentRepository', () => {
       describe('when called with non-existent id', () => {
         it('should throw an error', () => {
           const expectedMessage = 'some-message';
-          create = () => {throw new Error(expectedMessage)};
+          stubbedStreamFactory.create.throws(new Error(expectedMessage));
           // Act
           return (
             expect(firstValueFrom(repo.get(id+'x')))
@@ -97,7 +91,7 @@ describe('ApplicationEnvironmentRepository', () => {
       describe('when called with id that causes decoder to throw', () => {
         it('should throw an error', () => {
           const expectedMessage = 'some-message';
-          decode = () => {throw new Error(expectedMessage)};
+          stubbedCoder.decode.throws(new Error(expectedMessage));
           // Act
           return (
             expect(firstValueFrom(repo.get(id)))
@@ -110,11 +104,9 @@ describe('ApplicationEnvironmentRepository', () => {
         let expectedMessage: string;
         beforeEach(() => {
           expectedMessage = 'some-message';
-          read = stub().returns(of('1', '2'));
-          const temp = stub();
-          temp.onCall(0).returns(appEnv);
-          temp.onCall(1).throws(new Error(expectedMessage));
-          decode = temp;
+          stubbedStream.read.returns(of('1', '2'));
+          stubbedCoder.decode.onCall(0).returns(appEnv);
+          stubbedCoder.decode.onCall(1).throws(new Error(expectedMessage));
         });
         it('should call error handler once', async () => {
           await lastValueFrom(repo.get(id));
